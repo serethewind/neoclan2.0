df0256c7d523ff8f390e2f6981477d6100c7d150
serethewind <osasereu@gmail.com>
2023-07-30 21:10:30 +0100

    create EmailDetails dto. The message format that will be published to the queues

---
+++ transaction-service/transaction-service/src/main/java/com/neoclan/transactionservice/dto/communication/EmailDetails.java
@@ -0,0 +1,18 @@
+package com.neoclan.transactionservice.dto.communication;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class EmailDetails {
+
+    private String recipient;
+    private String message;
+    private String subject;
+    private String attachment;
+}
