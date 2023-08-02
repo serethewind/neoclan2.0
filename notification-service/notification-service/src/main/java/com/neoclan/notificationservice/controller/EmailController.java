53e0a049eb2a4d3ee2b736a832c9641b624296a6
serethewind <osasereu@gmail.com>
2023-08-02 13:27:52 +0100

    Api gateway microservice implementation

--- notification-service/notification-service/src/main/java/com/neoclan/notificationservice/controller/EmailController.java
+++ notification-service/notification-service/src/main/java/com/neoclan/notificationservice/controller/EmailController.java
@@ -5,10 +5,7 @@ import com.neoclan.notificationservice.dto.Response;
 import com.neoclan.notificationservice.service.EmailService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(("api/v2/email"))
@@ -16,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailController {
 
     private EmailService emailService;
+
+    @GetMapping("testing")
+    public ResponseEntity<String> checkTesting(){
+        return ResponseEntity.ok("Testing api gateway");
+    }
 
     @PostMapping("simpleMessage")
     public ResponseEntity<String> sendSimpleMessage(@RequestBody EmailDetails emailDetails){
