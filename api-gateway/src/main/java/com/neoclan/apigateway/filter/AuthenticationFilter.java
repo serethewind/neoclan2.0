7188fcd67a9b40a1b87374dd4f3c075b8200918b
serethewind <osasereu@gmail.com>
2023-08-03 03:25:08 +0100

    API Gateway receives Authentication Filter, Route Validator, JwtService with a validate token method. The application properties is configured and each route is protected by setting filter to Authentication Filter

---
+++ api-gateway/src/main/java/com/neoclan/apigateway/filter/AuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.neoclan.apigateway.filter;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+    @Autowired
+    private RouteValidator routeValidator;
+
+    @Autowired
+    private JwtService jwtService; //we will use the token-validate method of jwt service
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            if (routeValidator.isSecured.test(exchange.getRequest())) {
+                //if header does not contain the key 'Authorization'
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new RuntimeException("Missing authorization header");
+                }
+
+                //since header contains the key, get the value mapped to that key and assign it to auth header
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+
+                //if header is not null and header starts with 'Bearer ' because value ought to appear as 'Bearer <token string>'
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+                //Authheader contains token and token is to be validated
+                try {
+
+                    //Instance of jwtService with a validate method is also in the identity management service. note that the secret used in
+                    //creating the token in the identity management service is the same secret used in validating.
+                    //once token is valid, allow request to the corresponding micro-service
+                    jwtService.validateToken(authHeader);
+                } catch  (ExpiredJwtException | MalformedJwtException | SecurityException | IllegalArgumentException e){
+//                    throw new RuntimeException(e);
+
+                    ServerHttpResponse response = exchange.getResponse();
+                    response.setStatusCode(HttpStatus.BAD_REQUEST);
+                    return response.setComplete();
+                }
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    public static class Config {
+
+    }
+}
